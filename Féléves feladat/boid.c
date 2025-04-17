4dff05091c584f745a57820216eb05fd5a483650
LosoncziDominik <146641800+LosoncziDominik@users.noreply.github.com>
2025-04-17 11:57:18 +0200

    Add files via upload
    update

--- Féléves feladat/boid.c
+++ Féléves feladat/boid.c
@@ -1,11 +1,12 @@
 #include "boid.h"
-#include <math.h>
-#include <stdlib.h>
+
 
 Boid boids[NUM_BOIDS];
 Player player;
 Blood blood_drops[MAX_BLOOD];
 int bloodCount = 0;
+Circle circles[CIRCLE_AMOUNT];
+Color color;
 
 
 void init_boids() {
@@ -15,6 +16,13 @@ void init_boids() {
         boids[i].vx = ((rand() % 200) / 100.0f - 1) * MAX_SPEED;
         boids[i].vy = ((rand() % 200) / 100.0f - 1) * MAX_SPEED;
         boids[i].alive = true;
+        boids[i].is_scared = true;
+        boids[i].friendly = false;
+        boids[i].is_fleeing = false;
+        boids[i].is_coming = false;
+        boids[i].skin_color.r = 255;
+        boids[i].skin_color.g = 255;
+        boids[i].skin_color.b = 255;
     }
 }
 
@@ -23,21 +31,25 @@ void init_player(){
     player.y = SCREEN_HEIGHT * 3 / 4;
     player.vx = 0;
     player.vy = 0;
+    player.is_doomguy = true;
 }
 
-void update_boids(SDL_Renderer* renderer) {
+void update_boids() {
     for (int i = 0; i < NUM_BOIDS; i++) {
         float avgX = 0, avgY = 0;
         float avgVX = 0, avgVY = 0;
         float sepX = 0, sepY = 0;
         int count = 0;
+        boids[i].is_fleeing = false;
+        boids[i].is_coming = false;
+        boids[i].skin_color = color.white;
 
         for (int j = 0; j < NUM_BOIDS; j++) {
             if (i != j) {
                 float dx = boids[j].x - boids[i].x;
                 float dy = boids[j].y - boids[i].y;
                 float dist = sqrt(dx * dx + dy * dy);
-                
+
                 float pdx = player.x - boids[i].x;
                 float pdy = player.y - boids[i].y;
                 float pdist = sqrt(pdx * pdx + pdy * pdy);
@@ -56,12 +68,52 @@ void update_boids(SDL_Renderer* renderer) {
                 }
 
                 if (pdist < VIEW_RADIUS){
-                    float runx = -pdx / pdist;
-                    float runy = -pdy / pdist;
-
-                    boids[i].vx = runx * 1.5;
-                    boids[i].vy = runy * 1.5;
+                    if(boids[i].is_scared){
+                        boids[i].is_fleeing = true;
+                        set_skin_color(i);
+                        float runx = -pdx / pdist;
+                        float runy = -pdy / pdist;
+                        boids[i].vx = runx * 1.5;
+                        boids[i].vy = runy * 1.5;
+                    } else if (boids[i].friendly){
+                        boids[i].is_coming = true;
+                        set_skin_color(i);
+                        float runx = pdx / pdist;
+                        float runy = pdy / pdist;
+                        boids[i].vx = runx * 1.5;
+                        boids[i].vy = runy * 1.5;
+                    }
                 }
+            }
+        }
+
+        for (int k = 0; k < CIRCLE_AMOUNT; k++) {
+            float dx = circles[k].x - boids[i].x;
+            float dy = circles[k].y - boids[i].y;
+            float dist = sqrt(dx * dx + dy * dy);
+
+            float avoid_radius = circles[k].radius + 10;
+            if (dist < avoid_radius && dist > 5.0f) {
+                float vx = boids[i].vx;
+                float vy = boids[i].vy;
+                float speed = sqrt(vx * vx + vy * vy);
+                if (speed < 0.001f) speed = 0.001f;
+            
+                float sideX = vy;
+                float sideY = -vx;
+            
+                float scale = (1.0f - dist / avoid_radius) * 0.8f;
+                boids[i].vx += sideX * scale;
+                boids[i].vy += sideY * scale;
+            }
+            
+            if (dist < circles[k].radius) {
+                float dx = boids[i].x - circles[k].x;
+                float dy = boids[i].y - circles[k].y;
+                float pullout_strength = 0.02f;
+            
+                boids[i].vx += dx * pullout_strength;
+                boids[i].vy += dy * pullout_strength;
             }
         }
 
@@ -100,21 +152,20 @@ void update_boids(SDL_Renderer* renderer) {
 
         warp(&boids[i].x, &boids[i].y);
 
-        if (fabs(boids[i].x - player.x ) < 10 && fabs(boids[i].y - player.y ) < 10){
-            boids[i].alive = false;
-
-            if(bloodCount < MAX_BLOOD){
-                blood_drops[bloodCount].x = boids[i].x + rand() % 10 - 3;;
-                blood_drops[bloodCount].y = boids[i].y + rand() % 10 - 3;;
-                blood_drops[bloodCount].active = true;
-                bloodCount++;
+        if (player.is_doomguy) {
+            if (fabs(boids[i].x - player.x ) < 15 && fabs(boids[i].y - player.y ) < 15){
+                boids[i].alive = false;
+                if(bloodCount < MAX_BLOOD){
+                    blood_drops[bloodCount].x = boids[i].x + rand() % 10 - 3;
+                    blood_drops[bloodCount].y = boids[i].y + rand() % 10 - 3;
+                    blood_drops[bloodCount].active = true;
+                    bloodCount++;
+                }
             }
         }
-
-       
-
     }
 }
+
 
 void update_player() {
     const Uint8* keystates = SDL_GetKeyboardState(NULL);
@@ -137,7 +188,6 @@ void update_player() {
         player.y += vy;
 
         player.angle = atan2(player.vy, player.vx);
-
     }
 
     warp(&player.x, &player.y);
@@ -147,10 +197,10 @@ void update_player() {
 
 
 void render_boids(SDL_Renderer* renderer) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     
     for (int i = 0; i < NUM_BOIDS; i++) {
         if(boids[i].alive == true){
+            SDL_SetRenderDrawColor(renderer, boids[i].skin_color.r, boids[i].skin_color.g, boids[i].skin_color.b, 255);
 
             float angle = atan2(boids[i].vy, boids[i].vx);
 
@@ -161,7 +211,6 @@ void render_boids(SDL_Renderer* renderer) {
             SDL_Point triangle[4] = { p1, p2, p3, p1 };
             SDL_RenderDrawLines(renderer, triangle, 4);
         }
-        
     }
 }
 
@@ -218,6 +267,193 @@ void render_blood(SDL_Renderer* renderer){
     }
 }
 
-    int get_player_score(){
+int get_player_score(){
     return player.counter;
 }
+
+void render_dropdown_menu(SDL_Renderer* renderer, TTF_Font* font, Dropdown_Menu* ddm[], int menu_count) {
+    SDL_Color white = {255, 255, 255, 255};
+
+    for (int i = 0; i < menu_count; i++) {
+        if (!ddm[i]) continue;
+
+        int x = ddm[i]->x + (i * 170);
+        int y = ddm[i]->y;
+        int w = ddm[i]->w;
+        int h = ddm[i]->h;
+
+        SDL_Rect menu = {x, y, w, h};
+
+        if(ddm[i]->is_active){
+            SDL_SetRenderDrawColor(renderer, 70, 70, 70, 255);
+        }
+        else{
+            SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+        }
+        
+        SDL_RenderFillRect(renderer, &menu);
+        SDL_RenderDrawRect(renderer, &menu);
+
+        SDL_Surface* surface = TTF_RenderText_Blended(font, ddm[i]->options[ddm[i]->selected], white);
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_Rect text_rect = {x + 5, y + 5, surface->w, surface->h};
+        SDL_RenderCopy(renderer, texture, NULL, &text_rect);
+        SDL_FreeSurface(surface);
+        SDL_DestroyTexture(texture);
+
+        if (ddm[i]->open) {
+            for (int j = 0; j < ddm[i]->option_count; j++) {
+                SDL_Rect opt = {x, y + h * (j + 1), w, h};
+                SDL_SetRenderDrawColor(renderer, (j == ddm[i]->selected ? 80 : 30), 30, 30, 255);
+                SDL_RenderFillRect(renderer, &opt);
+                SDL_RenderDrawRect(renderer, &opt);
+
+                surface = TTF_RenderText_Blended(font, ddm[i]->options[j], white);
+                texture = SDL_CreateTextureFromSurface(renderer, surface);
+                text_rect = (SDL_Rect){x + 5, opt.y + 5, surface->w, surface->h};
+                SDL_RenderCopy(renderer, texture, NULL, &text_rect);
+                SDL_FreeSurface(surface);
+                SDL_DestroyTexture(texture);
+            }
+        }
+    }
+}
+
+void settings(Dropdown_Menu* ddm){
+    if(ddm->menuID == 1){
+        if(ddm->selected == 0){
+            player.is_doomguy = false;
+            }
+            else{
+            player.is_doomguy = true;
+            }
+    }
+    else if (ddm->menuID == 2){
+        if(ddm->selected == 0){
+            for (int i = 0; i < NUM_BOIDS; i++)
+            {
+                boids[i].is_scared = false;
+                boids[i].friendly = true;
+            }
+            
+
+        }
+        else if (ddm->selected == 1){
+            for (int i = 0; i < NUM_BOIDS; i++)
+            {
+                boids[i].is_scared = false;
+                boids[i].friendly = false;
+            }
+        }
+        else{
+            for (int i = 0; i < NUM_BOIDS; i++)
+            {
+                boids[i].is_scared = true;
+                boids[i].friendly = false;
+            }
+        }
+    }
+}
+
+void display_guide(SDL_Renderer* renderer, TTF_Font* font) {
+    FILE* file = fopen("guide.txt", "r");
+    if (!file) {
+        perror("can't open guide.txt");
+        return;
+    }
+
+    SDL_Color white = {0, 0, 0, 255};
+    SDL_Color bg = {255, 255, 255, 150};
+
+    char line[256];
+    int y = 100;
+
+    while (fgets(line, sizeof(line), file)) {
+        size_t len = strlen(line);
+        if (len > 0 && line[len - 1] == '\n') {
+            line[len - 1] = '\0';
+        }
+
+        SDL_Surface* surface = TTF_RenderText_Blended(font, line, white);
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+        SDL_Rect text_rect = {40, y, surface->w, surface->h};
+
+        SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+        SDL_SetRenderDrawColor(renderer, bg.r, bg.g, bg.b, bg.a);
+        SDL_RenderFillRect(renderer, &text_rect);
+
+        SDL_RenderCopy(renderer, texture, NULL, &text_rect);
+        y += surface->h;
+
+        SDL_FreeSurface(surface);
+        SDL_DestroyTexture(texture);
+    }
+
+    fclose(file);
+}
+
+void draw_circle_outline(SDL_Renderer* renderer, int cx, int cy, int radius, int segments, int dash_ratio) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+    double angle_step = 2 * 3.14 / segments;
+    for (int i = 0; i < segments; i += dash_ratio * 2) {
+        for (int j = 0; j < dash_ratio && (i + j + 1) < segments; ++j) {
+            double angle1 = (i + j) * angle_step;
+            double angle2 = (i + j + 1) * angle_step;
+
+            int x1 = cx + (int)(radius * cos(angle1));
+            int y1 = cy + (int)(radius * sin(angle1));
+            int x2 = cx + (int)(radius * cos(angle2));
+            int y2 = cy + (int)(radius * sin(angle2));
+
+            SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+        }
+    }
+}
+
+void circle_init(int center_x, int center_y, int radius, int circle_count){
+
+    circles[circle_count].x = (double)center_x;
+    circles[circle_count].y = (double)center_y;
+    if(radius > 100) radius = 100;
+    circles[circle_count].radius = (double)radius;
+    circles[circle_count].color = color.white;
+}
+
+int get_circle_x(int index){
+    return circles[index].x;
+}
+
+int get_circle_y(int index){
+    return circles[index].y;
+}
+
+int get_circle_radius(int index){
+    return circles[index].radius;
+}
+
+void set_skin_color(int index){
+    if(boids[index].is_fleeing){
+        boids[index].skin_color = color.yellow;
+    } 
+    else if (boids[index].is_coming){
+        boids[index].skin_color = color.green;
+    } 
+    else boids[index].skin_color = color.white;
+
+    
+}
+
+void init_color(){
+    SDL_Color white  = {255, 255, 255, 255};
+    SDL_Color green  = {0,   255,   0, 255};
+    SDL_Color red    = {255,   0,   0, 255};
+    SDL_Color yellow = {255, 255,   0, 255};
+
+    color.white  = white;
+    color.green  = green;
+    color.red    = red;
+    color.yellow = yellow;
+}
+
+
